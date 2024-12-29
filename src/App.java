c12f3289c494e776a8fa88f6d5493cd63a8b0172
Tsitohaina21 <tsitoramanamidona@gmail.com>
2024-12-29 11:21:47 +0300

    crud

--- src/App.java
+++ src/App.java
@@ -4,9 +4,9 @@ import entity.TypeProduit;
 
 public class App {
     public static void main(String[] args) {
-        List<TypeProduit> typeProduits = TypeProduit.getAll();
-        for (TypeProduit typeProduit : typeProduits) {
-            System.out.println(typeProduit.getId() + " " + typeProduit.getLabel());
-        }
+        // List<TypeProduit> typeProduits = TypeProduit.getAll();
+        // for (TypeProduit typeProduit : typeProduits) {
+        //     System.out.println(typeProduit.getId() + " " + typeProduit.getLabel());
+        // }
     }
 }
